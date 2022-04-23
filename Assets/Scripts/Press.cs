92cd4610fd083f0808ec72c100fd4f7d9125df40
syl3n7 <claudiosilva@protonmail.com>
2022-04-23 17:32:29 +0100

    update

--- Assets/Scripts/Press.cs
+++ Assets/Scripts/Press.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Press : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
